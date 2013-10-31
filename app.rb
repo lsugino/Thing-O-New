7a4f10002c13085c7ff038fab1c26a5376eccdc2
Apprentice <apprentice@dbc02.local>
2013-10-30 23:36:31 -0700

    added html and css skeleton + unchecked app.rb

--- app.rb
+++ app.rb
@@ -6,12 +6,49 @@ ActiveRecord::Base.establish_connection(adapter: 'postgresql',
 									database: 'thing_o_new_dev')
 
 get '/' do
-	@all_users = User.all # :order => :id 
+	@all_users = User.all # :order => :id
   erb :homepage
 end
 
 post '/' do  
-  User.create(:name => params[:name])
+  User.create!(:name => params[:name])
   redirect '/'
 end 
 
+# restful stuff for dog
+# INDEX READ
+# index.html.erb
+get "/dogs" do
+end
+
+# READ
+# contains form to create dog
+# new.html.erb
+get "/dog/new" do
+end
+
+# CREATE
+post "/dog" do
+end
+
+# READ
+# show.html.erb
+get "/dog" do
+end
+
+# EDIT
+# edit.html.erb
+# contains form to update a existing entry
+get "/dog/edit/:id" do
+  @dog = Dog.find(params[:id])
+end
+
+# UPDATE
+put "/dog" do
+
+end
+
+# DELETE
+delete "/dog" do
+  Dog.destroy(params[:id])
+end
